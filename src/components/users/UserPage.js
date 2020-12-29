bc805b69b0a81f9396ebf533034d59f8d66f67c2
ckashby <clay.ashby@gmail.com>
2020-12-29 09:36:24 -0800

    add styled-components

--- src/components/users/UserPage.js
+++ src/components/users/UserPage.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
 import Navbar from "../layout/Navbar";
 import Users from "./Users";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background-color: salmon;
+  margin: auto;
+  padding: 1em;
+`;
+
+const MyButt = styled.button`
+  color: yellow;
+  font-size: 24px;
+  font-weight: bold;
+  min-width: fit-content;
+  margin: 32px;
+  background-color: blue;
+  padding: 8px;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px grey;
+`;
+
+const Title = styled.h1`
+  color: green;
+  font-weight: bold;
+  font-size: 2em;
+  text-align: center;
+`;
+
+const alertMe = () => {
+  alert("No is the time...");
+};
 
 export class UserPage extends Component {
   render() {
@@ -8,6 +38,10 @@ export class UserPage extends Component {
       <div>
         <Navbar />
         <div className="container">
+          <MyButt onClick={alertMe}>Hola mi amigos</MyButt>
+          <Wrapper>
+            <Title>Welcome</Title>
+          </Wrapper>
           <Users />
         </div>
       </div>
